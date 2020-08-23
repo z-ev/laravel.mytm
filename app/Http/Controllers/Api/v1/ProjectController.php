66b480d07a0bd33ca065e51f488f37c810cee515
evgenii <evgenii@zabairachnyi.com>
2020-08-24 00:50:06 +0400

    add project resource (create, update, destroy)

--- app/Http/Controllers/Api/v1/ProjectController.php
+++ app/Http/Controllers/Api/v1/ProjectController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Exceptions\UserNotSignUp;
+use App\Http\Requests\ProjectCreateRequest;
+use App\Http\Requests\ProjectUpdateRequest;
+use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
+
 
 class ProjectController extends Controller
 {
@@ -24,29 +31,59 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+
+
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Создаем проект
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * (post) /projects/{id}
+     *
+     * @param ProjectCreateRequest $request
+     * @return ProjectResource
      */
-    public function store(Request $request)
+    public function store(ProjectCreateRequest $request)
     {
-        //
+        $project = new Project();
+
+        $title = request('title');
+        $body = request('body');
+
+
+        $deadline = Carbon::parse(request('deadline'));
+        $project->user_id = auth()->user()->id;
+
+        $project->title = $title;
+
+        if (isset($body)) {
+            $project->body = $body;
+        } else $project->body = '';
+
+        if (isset($deadline)) {
+            $project->deadline = $deadline;
+
+        } else $project->deadline = Carbon::parse('2020-08-23');
+
+        $project->status = '1';
+
+        $project->save();
+
+        return new ProjectResource($project);
     }
 
+
     /**
-     * Display the specified resource.
+     * Информация о проекте по ИД
      *
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
+     * (get) /projects/{id}
+     *
+     * @param $id
      */
-    public function show(Project $project)
+    public function show($id)
     {
-        //
+
+
     }
 
     /**
@@ -61,25 +98,70 @@ class ProjectController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Обновить проект
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Project  $project
-     * @return \Illuminate\Http\Response
+     * (patch) /projects
+     *
+     * @param ProjectUpdateRequest $request
+     * @param $id
+     * @return ProjectResource
+     * @throws UserNotSignUp
      */
-    public function update(Request $request, Project $project)
+    public function update(ProjectUpdateRequest $request, $id)
     {
-        //
+        $project = Project::findOrFail($id);
+
+            if (isset($request['title'])) {
+                $project->title = $request['title'];
+            }
+
+            if (isset($request['body'])) {
+                $project->body = $request['body'];
+            }
+
+            if (isset($request['deadline'])) {
+                $project->deadline = Carbon::parse($request['deadline']);
+            } else $project->deadline = Carbon::parse('2020-08-23 16:53:23');
+
+            $project->save;
+
+            return new ProjectResource($project);
+
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Удаляем все задачи пользователя по ИД проекта
+     *
+     * (delete) /projects/{id}
      *
      * @param  \App\Models\Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Project $project)
+    public function destroy($id)
     {
-        //
+        $project = Project::Find($id);
+
+        if (!$project) {
+            $error = 'Такого проекта не существует';
+            return response()->json(['data' => ['error' => $error]], 400);
+        }
+        if ($project->user_id != auth()->user()->id) {
+            $error = 'У вас нет прав для удаления проекта';
+        } else {
+
+            /*$tasks = Task::where('project_id', $id)->first();
+            if ($tasks) {
+                $error = 'Не возможно удалить проект та как по нему поставлены задачи';
+            }*/
+        }
+        if (isset($error)) {
+            return response()->json(['data' => ['error' => $error]], 400);
+        } else {
+
+            $project->delete();
+            return response()->json(['data' => ['message' => 'Проект успешно удален!'], 'links' => [
+                'self' => route('api.projects.index'),
+            ]], 200);
+        }
     }
 }
---
+++ app/Http/Requests/ProjectUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjectUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'sometimes|min:5|max:300',
+            'body' => 'sometimes|max:800|min:10',
+            'deadline' => 'sometimes|date',
+        ];
+    }
+}
