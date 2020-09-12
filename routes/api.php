a4b66c83e303f2ea89db43754d1edc864a7379d1
evgenii <evgenii@zabairachnyi.com>
2020-09-13 03:00:51 +0400

    style(routes): update

--- routes/api.php
+++ routes/api.php
@@ -15,57 +15,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 /*
-    +-----------+-------------------------+------------------+-----------------------++++-------------------------+
-    | Method    | URI                     | Name             | Action                                             |
-    +-----------+-------------------------+------------------+----------------------------------------------------+
-    | GET|HEAD  | /                       |                  | Closure                                            |
-    | GET|HEAD  | projects                | projects.index   | App\Http\Controllers\Api\v1\UserController@index   |
-    | POST      | projects                | projects.store   | App\Http\Controllers\Api\v1\UserController@store   |
-    | GET|HEAD  | projects/create         | projects.create  | App\Http\Controllers\Api\v1\UserController@create  |
-    | GET|HEAD  | projects/{article}      | projects.show    | App\Http\Controllers\Api\v1\UserController@show    |
-    | PUT|PATCH | projects/{article}      | projects.update  | App\Http\Controllers\Api\v1\UserController@update  |
-    | DELETE    | projects/{article}      | projects.destroy | App\Http\Controllers\Api\v1\UserController@destroy |
-    | GET|HEAD  | projects/{article}/edit | projects.edit    | App\Http\Controllers\Api\v1\UserController@edit    |
-    +-----------+-------------------------+------------------+----------------------------------------------------+
+    +-----------+-------------------------+------------------+-----------------------++++----------------------------+
+    | Method    | URI                     | Name             | Action                                                |
+    +-----------+-------------------------+------------------+-------------------------------------------------------+
+    | GET|HEAD  | /                       |                  | Closure                                               |
+    | GET|HEAD  | projects                | projects.index   | App\Http\Controllers\Api\v1\ProjectController@index   |
+    | POST      | projects                | projects.store   | App\Http\Controllers\Api\v1\ProjectController@store   |
+    | GET|HEAD  | projects/create         | projects.create  | App\Http\Controllers\Api\v1\ProjectController@create  |
+    | GET|HEAD  | projects/{article}      | projects.show    | App\Http\Controllers\Api\v1\ProjectController@show    |
+    | PUT|PATCH | projects/{article}      | projects.update  | App\Http\Controllers\Api\v1\ProjectController@update  |
+    | DELETE    | projects/{article}      | projects.destroy | App\Http\Controllers\Api\v1\ProjectController@destroy |
+    | GET|HEAD  | projects/{article}/edit | projects.edit    | App\Http\Controllers\Api\v1\ProjectController@edit    |
+    +-----------+-------------------------+------------------+-------------------------------------------------------+
 */
 
 // Создать пользователя
 Route::post('signup', 'Api\v1\UserController@signUp')->name('users.signup');
 // Получить новый токен
-Route::post('signin', 'Api\v1\UserController@signIn')->name('users.signin');;
+Route::post('signin', 'Api\v1\UserController@signIn')->name('users.signin');
 
-Route::middleware('auth:api')->group(function(){
-
+Route::middleware('auth:api')->group(function () {
     //Сменить токен
     Route::get('signout', 'Api\v1\UserController@signOut')->name('users.signout');
-
-    /**
-     * Информация о всех пользователях, их проектах и задачах с пагинацией
-     */
-
     // Информация о пользователе
     Route::get('/info', 'Api\v1\UserController@infoUser')->name('users.info');
 
     Route::resource('users', 'Api\v1\UserController');
-
     Route::resource('projects', 'Api\v1\ProjectController');
-
     Route::resource('tasks', 'Api\v1\TaskController');
 
-    Route::delete('projects/{id}/kill', 'Api\v1\ProjectController@kill');;
+    Route::delete('projects/{id}/kill', 'Api\v1\ProjectController@kill');
 
-    /**
-     * Elasticsearch
-     */
+    // Elasticsearch
     Route::prefix('/search')->group(function () {
         // Поиск по ключевому слову ?ser=word...
         Route::get('/', 'Api\v1\SearchController@find')->name('search');
-
         // Добавление документов в Elastic
         Route::post('/', 'Api\v1\SearchController@createIndex');
         // Удаление документов из Elastic
         Route::delete('/', 'Api\v1\SearchController@deleteIndex');
-
     });
-
 });
